b1bb4cc8606fdd66059ded5fbecb30eedd5b1b4f
chosm10 <pelto@naver.com>
2018-01-18 00:49:29 +0900

    이벤트 클래스 수정

--- IOTA/src/Event.java
+++ IOTA/src/Event.java
@@ -1,34 +1,29 @@
 
 public class Event {
-	EventElement eventelement;  // EventElement 인터페이스를 구현한 필드 클래스와 타이머 클래스를 인자로 받음.
-	String TriggerCondition;
-	public Event(EventElement eventelement, String TriggerCondition) {
-		this.eventelement = eventelement;
-		this.TriggerCondition = TriggerCondition;
+	EventElement eventElement;  // EventElement 인터페이스를 구현한 필드 클래스와 타이머 클래스를 인자로 받음.
+	
+	public Event(EventElement eventElement) {
+		this.eventElement = eventElement;
 	}
-	public Boolean IsEventTriggered() {
+	public Boolean IsEventTriggered() { // 전, 후 값 상관 없이 값이 변했는지
 
-		if(TriggerCondition == ".->") { // f[.->], m[.->]
-				if(eventelement.GetOldValue() != eventelement.GetNewValue())
-					return true;
-				else
-					return false;
+		if(eventElement.GetOldValue() != eventElement.GetNewValue())
+			return true;
+		else
+			return false;
+	}
+	public Boolean IsEventTriggered(String n) { // 필드나 타이머의 값의 전 값이 n에서 바꼈는지
+		
+		if((eventElement.GetOldValue() == n) && (eventElement.GetOldValue() != eventElement.GetNewValue())) {
+			return true;
 		}
-		else if(TriggerCondition == "n->" && eventelement.GetOldValue() == "n") { // f[n->], m[n->]
-				if(eventelement.GetOldValue() != eventelement.GetNewValue())
-					return true;
-				else
-					return false;
-		}
-		else if(TriggerCondition == "n1->n2") {
-			
-		}
-
-	
-
-
-	
-	return eventelement.GetOldValue() == eventelement.GetNewValue();
-}
-
+		else
+			return false;
+	}
+	public Boolean IsEventTirggered(String n1, String n2) { // 필드나 타이머의 값의 바뀌기 전 값과 바뀐 후 값이 n1에서 n2로 바꼈는지
+		if((eventElement.GetOldValue() == n1) && (eventElement.GetNewValue() == n2))
+			return true;
+		else
+			return false;
+	}
 }
--- IOTA/src/EventHandler.java
+++ IOTA/src/EventHandler.java
@@ -5,5 +5,8 @@ public class EventHandler {
 	public EventHandler(Event e) {
 		event = e;
 	}
+	public void CheckEvent() {
+		
+	}
 	
 }
